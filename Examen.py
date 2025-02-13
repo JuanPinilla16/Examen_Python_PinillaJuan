2bbf99311fa1c42ce014afe07a6b387458e30081
Juanu12 <juanferumana@hotmail.com>
2025-02-13 09:42:30 -0500

    Examen

--- Examen.py
+++ Examen.py
@@ -14,21 +14,43 @@ Servicios={}
 booleanito = True
 while (booleanito==True):
 
-    print("####################################")
-    print("####---Bienvenido a movistar---####")
-    print("####################################")
-    print("¿Que te gustaria hacer?")
-    print("1.Ver Servicios Actuales")
-    print("2.Ver Usuarios")
-    print("3.Modificar Usuarios")
-    print("4.Cerrar Programa")
+    print("############################")
+    print("#####---Bienvenido A Movistar---#######")
+    print("############################")
+    print("¿A que te gustaria Ingresar?")
+    print("1. Coordinador")
+    print("2. Usuario")
+    print("3. Registrarse")
+    print("4. Cerrar programa")
     opt=input(":")
+    ##Leer archivo JSON y pasarlo a dic
+    Servicios=abrirJSON()
+    if(opt=="1"):
+        print("¿Qué te gustaría hacer?")
+        print("1.Servicios")
+        opt2=int(input(":"))
+        if (opt2==1):
+            print("¿Qué te gustaría hacer?")
+            print("1.Verlos")
+            print("2.Agregar uno nuevo")
+            print("3.Modificar uno")
+            print("4.Eliminar uno")
+            opt3=int(input(":"))
+            if(opt3==1):
+                print("############################")
+                print("#####---Servicios Disponibles---#######")
+                print("############################")
+                for i in range(len(Servicios[opt]["Megas"])):
+                    print(Servicios[opt]["Megas"][i+1])
+                print("############################")
+            elif(opt3==2):
+                nuevoServicio=input("¿Como se llama el nuevo Servicio?:")
+                Servicios[opt]["servicio"].append(nuevoServicio)
+                #Guardar diccionario en el archivo JSON
+                guardarJSON(Servicios)
+                print("Servicio Agregado")
+    elif(opt=="4"):
+        booleanito = False
+        break
+            
 
-
-
-Servicios=abrirJSON()
-opt2=int(input(":"))
-if(opt2==1):
-    print("Servicios Disponibles")
-    for i in range (len(Servicios[opt]["servicios"])):
-        print(Servicios[opt]["servicios"][i])
